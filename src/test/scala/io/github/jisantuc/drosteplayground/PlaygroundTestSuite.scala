18e3813beaa4c1df0ec3a877c6c31b549fbe8769
James Santucci <james.santucci@47deg.com>
2022-01-11 14:57:25 -0700

    Solve the puzzle with recursion schemes instead (#1)
    * wip -- unclear how to construct algebra
    
    * add ci
    
    * oops bad copy for .github
    
    * add jdk to shell.nix
    
    * implement and test position adjustment algebra

--- src/test/scala/io/github/jisantuc/drosteplayground/PlaygroundTestSuite.scala
+++ src/test/scala/io/github/jisantuc/drosteplayground/PlaygroundTestSuite.scala
@@ -1,8 +1,11 @@
 package io.github.jisantuc.drosteplayground
 
+import higherkindness.droste.scheme
 import munit.FunSuite
 
 import Playground._
+import higherkindness.droste.Algebra
+import higherkindness.droste.data.Fix
 
 class PlaygroundTestSuite extends FunSuite {
   test("solve the sample puzzle with explicit recursion") {
@@ -14,6 +17,37 @@ class PlaygroundTestSuite extends FunSuite {
 
     assertEquals(
       evalR(samplePuzzle)((HPosition(0), VPosition(0))),
+      (HPosition(15), VPosition(10))
+    )
+  }
+
+  test("solve the sample puzzle with rec schemes") {
+    // annotation is necessary here because without it we seem to hit
+    // some kind of recursion limit and the compiler gets mad about the
+    // EndF 😱
+    val samplePuzzle: Fix[InstructionF] = Fix(
+      HAdjustF(
+        5,
+        Fix(
+          VAdjustF(
+            5,
+            Fix(
+              HAdjustF(
+                8,
+                Fix(
+                  VAdjustF(-3, Fix(VAdjustF(8, Fix(HAdjustF(2, Fix(EndF()))))))
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+
+    val adjust = scheme.cata(algAdjust)
+
+    assertEquals(
+      adjust(samplePuzzle)((HPosition(0), VPosition(0))),
       (HPosition(15), VPosition(10))
     )
   }
